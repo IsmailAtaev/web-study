a8c3bd83470cce7fd614e6f86208e3efac3e9150
IsmailAtaev <ataewisma@gmail.com>
2022-12-25 01:14:35 +0300

    init: server

--- js/events.js
+++
@@ -1,9 +0,0 @@
-import { handleInput, handleSubmit } from "./convert.js";
-import { fetchCodes } from "./index.js";
-import variables from "./variables.js";
-
-const { amountInput, form } = variables;
-
-fetchCodes();
-amountInput.addEventListener("keyup", handleInput);
-form.addEventListener("submit", handleSubmit);
--- js/utils.js
+++
@@ -1,4 +0,0 @@
-export const getFullTitle = (codes, code) => {
-  const [, title] = codes.find((item) => item.includes(code));
-  return title;
-};
