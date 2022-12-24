2a7f7d4122f323522021831e734388360a127e5b
IsmailAtaev <ataewisma@gmail.com>
2022-12-24 16:35:52 +0300

    feat: add convertor task

---
+++ js/state.js
@@ -0,0 +1,10 @@
+export default {
+  url: "https://v6.exchangerate-api.com/v6/c35586c1e9a29f68a3f6604c",
+  codes: [],
+  pair: {
+    from: "",
+    to: "",
+  },
+  amount: "",
+  loading: false,
+};
